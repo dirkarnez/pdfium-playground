0ab24fa54bccf9c25a6147152e0017da3bc5875c
dirkarnez <smalldirkalex@gmail.com>
2022-08-05 06:15:17 +0800

    - init, working

---
+++ README.md
@@ -0,0 +1,9 @@
+pdfium-playground
+=================
+Based on [ikuokuo/pdfium-reader: PDFium Reader](https://github.com/ikuokuo/pdfium-reader)
+
+### Using
+- [bblanchon/pdfium-binaries: 📰 Binary distribution of PDFium](https://github.com/bblanchon/pdfium-binaries)
+
+### Reference
+- [PDFium 渲染 - SegmentFault 思否](https://segmentfault.com/a/1190000041060780)
---
+++ src/main.cpp
@@ -0,0 +1,156 @@
+#include <fpdfview.h>
+#include <fpdf_doc.h>
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+#include <codecvt>
+#include <locale>
+
+#include <cstring>
+#include <iomanip>
+#include <vector>
+
+#include <fpdf_attachment.h>
+#include <fpdf_edit.h>
+#include <fpdf_text.h>
+
+namespace strings {
+
+std::string FromUtf16(const std::u16string &u16str);
+
+bool EndsWith(const std::string &str, const std::string &suffix);
+bool StartsWith(const std::string &str, const std::string &prefix);
+
+std::size_t ReplaceAll(std::string &str,
+    const std::string &what, const std::string &with);
+
+}  // namespace strings
+
+namespace fpdf {
+
+void PrintLastError() {
+  unsigned long err = FPDF_GetLastError();
+  fprintf(stderr, "Load pdf docs unsuccessful: ");
+  switch (err) {
+    case FPDF_ERR_SUCCESS:
+      fprintf(stderr, "Success");
+      break;
+    case FPDF_ERR_UNKNOWN:
+      fprintf(stderr, "Unknown error");
+      break;
+    case FPDF_ERR_FILE:
+      fprintf(stderr, "File not found or could not be opened");
+      break;
+    case FPDF_ERR_FORMAT:
+      fprintf(stderr, "File not in PDF format or corrupted");
+      break;
+    case FPDF_ERR_PASSWORD:
+      fprintf(stderr, "Password required or incorrect password");
+      break;
+    case FPDF_ERR_SECURITY:
+      fprintf(stderr, "Unsupported security scheme");
+      break;
+    case FPDF_ERR_PAGE:
+      fprintf(stderr, "Page not found or content error");
+      break;
+    default:
+      fprintf(stderr, "Unknown error %ld", err);
+  }
+  fprintf(stderr, ".\n");
+}
+
+std::string DateToRFC3399(const std::string &str) {
+  if (str.empty()) return str;
+  std::string s = str;
+  if (strings::StartsWith(s, "D:")) {
+    s.erase(0, 2);
+  }
+  s.insert(4, "-");
+  s.insert(7, "-");
+  s.insert(10, "T");
+  s.insert(13, ":");
+  s.insert(16, ":");
+  strings::ReplaceAll(s, "'", ":");
+  if (strings::EndsWith(s, ":")) {
+    s.erase(s.size()-1);
+  }
+  return s;
+}
+
+}  // namespace fpdf
+
+namespace strings {
+
+// ref:
+//  https://en.cppreference.com/w/cpp/locale/codecvt_utf8_utf16
+//  https://en.cppreference.com/w/cpp/locale/wstring_convert/to_bytes
+std::string FromUtf16(const std::u16string &u16str) {
+  std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> conv;
+  return conv.to_bytes(u16str);
+}
+
+// ref: https://stackoverflow.com/a/42844629
+bool EndsWith(const std::string &str, const std::string &suffix) {
+  return str.size() >= suffix.size() &&
+      0 == str.compare(str.size()-suffix.size(), suffix.size(), suffix);
+}
+bool StartsWith(const std::string &str, const std::string &prefix) {
+  return str.size() >= prefix.size() &&
+      0 == str.compare(0, prefix.size(), prefix);
+}
+
+// ref: https://en.cppreference.com/w/cpp/string/basic_string/replace
+std::size_t ReplaceAll(std::string &inout,
+    const std::string &what, const std::string &with) {
+  std::size_t count{};
+  for (std::string::size_type pos{};
+      inout.npos != (pos = inout.find(what.data(), pos, what.length()));
+      pos += with.length(), ++count) {
+    inout.replace(pos, what.length(), with.data(), with.length());
+  }
+  return count;
+}
+
+}  // namespace strings
+
+using namespace std;
+
+void PrintPdfMetaData(FPDF_DOCUMENT doc) {
+  static constexpr const char *kMetaTags[] = {
+      "Title",   "Author",   "Subject",      "Keywords",
+      "Creator", "Producer", "CreationDate", "ModDate"};
+  for (const char *meta_tag : kMetaTags) {
+    const unsigned long len = FPDF_GetMetaText(doc, meta_tag, nullptr, 0);
+    if (!len)
+      continue;
+
+    std::vector<char16_t> buf(len);
+    FPDF_GetMetaText(doc, meta_tag, buf.data(), buf.size());
+    auto text = strings::FromUtf16(std::u16string(buf.data()));
+    if (strcmp(meta_tag, "CreationDate") == 0 ||
+        strcmp(meta_tag, "ModDate") == 0) {
+      text = fpdf::DateToRFC3399(text);
+    }
+    std::cout << " " << meta_tag << ": " << text << std::endl;
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  FPDF_STRING test_doc = argv[1];
+
+  FPDF_InitLibrary();
+
+  FPDF_DOCUMENT doc = FPDF_LoadDocument(test_doc, NULL);
+  if (!doc) {
+      unsigned long err = FPDF_GetLastError();
+      // Load pdf docs unsuccessful: ...
+      goto EXIT;
+  }
+
+   PrintPdfMetaData(doc);
+  FPDF_CloseDocument(doc);
+EXIT:
+  FPDF_DestroyLibrary();
+  return 0;
+}
